5085239f0c5bf4f389b5ceb35368e3d12bccaab2
previtus <previtus@gmail.com>
2020-02-18 22:08:36 +0000

    Week 2 upload, initial version of slides and code as a starter for task

---
+++ week02_math-recap/w02_plot_quadratic_function.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+x_values=[]
+y_values=[]
+
+# TODO: sample the x and y values!
+
+x = ...
+y = ...
+
+fig = plt.figure()
+axes=fig.add_subplot(111)
+axes.plot(x,y)
+plt.show()
